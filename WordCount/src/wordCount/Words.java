4f4afb799e32bf2932c60cddce1aad5f053bedbe
TinaDelli <cecurrier92@gmail.com>
2019-05-29 19:20:44 -0400

    forgot to commit but got the words to print and working on getting the index AND words to print

---
+++ WordCount/src/wordCount/Words.java
@@ -0,0 +1,28 @@
+package wordCount;
+
+public class Words
+{
+    private String words;
+
+    public Words(String words)
+    {
+        this.words= words;
+    }
+    
+    public String getWords()
+    {
+        return words;
+    }
+    public void setWords(String words)
+    {
+        this.words= words;
+    }
+
+    @Override
+    public String toString()
+    {
+        return "Words{" +
+                "words='" + words + '\'' +
+                '}';
+    }
+}
